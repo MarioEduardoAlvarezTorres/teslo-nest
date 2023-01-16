dfdbc43eda3593d5eef5565706537709a2b6eeda
MarioEduardo <eduardoat44@gmail.com>
2023-01-16 13:31:31 -0600

    SEED de usuarios, productos e imagenes

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { Auth } from 'src/auth/decorators';
 import { SeedService } from './seed.service';
-import { ValidRoles } from '../auth/interfaces/valid-roles';
 
 @Controller('seed')
 export class SeedController {
